393c1ec20fd6a13b708b4409f5c2a88cf412f414
Guillaume Chau <alphadelta.fg@gmail.com>
2019-06-17 22:11:17 +0200

    feat: router history mode

--- src/router.js
+++ src/router.js
@@ -6,27 +6,75 @@ import NotFound from './components/NotFound.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: PokemonList
-    },
-    {
-      path: '/pokemon/:num',
-      name: 'pokemon-details',
-      component: PokemonDetails,
-      props: route => {
-        return {
-          id: parseInt(route.params.num)
-        }
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: PokemonList
+  },
+  {
+    path: '/pokemon/:num',
+    name: 'pokemon-details',
+    component: PokemonDetails,
+    props: route => {
+      return {
+        id: parseInt(route.params.num)
       }
-    },
-    {
-      path: '*',
-      name: 'not-found',
-      component: NotFound
     }
-  ]
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
+  }
+]
+
+function scrollBehavior (to, from, savedPosition) {
+  if (savedPosition) {
+    // savedPosition is only available for popstate navigations.
+    return savedPosition
+  } else {
+    const position = {}
+
+    // scroll to anchor by returning the selector
+    if (to.hash) {
+      position.selector = to.hash
+
+      // specify offset of the element
+      if (to.hash === '#anchor2') {
+        position.offset = { y: 100 }
+      }
+
+      if (document.querySelector(to.hash)) {
+        return position
+      }
+
+      // if the returned position is falsy or an empty object,
+      // will retain current scroll position.
+      return false
+    }
+
+    return new Promise(resolve => {
+      // check if any matched route config has meta that requires scrolling to top
+      if (to.matched.some(m => m.meta.scrollToTop)) {
+        // coords will be used if no selector is provided,
+        // or if the selector didn't match any element.
+        position.x = 0
+        position.y = 0
+      }
+
+      // wait for the out transition to complete (if necessary)
+      this.app.$root.$once('triggerScroll', () => {
+        // if the resolved position is falsy or an empty object,
+        // will retain current scroll position.
+        resolve(position)
+      })
+    })
+  }
+}
+
+export default new Router({
+  mode: 'history',
+  routes,
+  scrollBehavior
 })
